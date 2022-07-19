548aa09ae989725dca99da425920070249386178
nidhi gupta <nidhitech0510@gmail.com>
2022-07-19 14:00:46 +0530

    commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import FlexDirectionBasics from './components/flex_Direction';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlexDirectionBasics></FlexDirectionBasics>
     </View>
+
+
   );
 }
 
